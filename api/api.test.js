6deee610be78b6f2fa7ccfcbd328aeec3961b345
Shidil Eringa <shidil@live.com>
2021-11-01 13:33:13 +0530

    Added e2e tests for api

--- api/api.test.js
+++ api/api.test.js
@@ -1,15 +1,53 @@
 const request = require("supertest");
 const { app } = require("./api");
+const db = require("../shared/dal");
 
-describe("root", () => {
-    test("/do-stuff with valid id", async () => {
+const randId = "test_rec_" + Math.random();
+
+describe("/do-stuff non existent", () => {
+    test("value for non existent id results in 1", async () => {
         await request(app)
-            .post("/do-stuff", { id: "test_rec" })
+            .post("/do-stuff")
+            .send({ id: randId })
             .expect(200)
             .then((res) => {
                 expect(res.body).toEqual({
                     success: true,
                     result: 1,
+                });
+            });
+    });
+});
+
+describe("/do-stuff existing", () => {
+    test("odd values gets incremented by 1", async () => {
+        await request(app)
+            .post("/do-stuff")
+            .send({ id: randId })
+            .expect(200)
+            .then((res) => {
+                expect(res.body).toEqual({
+                    success: true,
+                    result: 2,
+                });
+            });
+    });
+});
+
+describe("/do-stuff existing even", () => {
+    afterAll(() => {
+        db.tearDown();
+    });
+
+    test("even values gets incremented by 1", async () => {
+        await request(app)
+            .post("/do-stuff")
+            .send({ id: randId })
+            .expect(200)
+            .then((res) => {
+                expect(res.body).toEqual({
+                    success: true,
+                    result: 5,
                 });
             });
     });
