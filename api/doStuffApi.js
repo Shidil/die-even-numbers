c5cd5a9f03fe98df2faa75fda0cd6d4f00e4df84
Shidil Eringa <shidil@live.com>
2021-11-01 13:20:59 +0530

    Don't crash for non existant id

--- api/doStuffApi.js
+++ api/doStuffApi.js
@@ -3,7 +3,7 @@ const { increment } = require("../shared/increment");
 
 const doStuff = async function (id) {
     const current = await getCurrentValue(id);
-    const nextValue = increment(current.value);
+    const nextValue = increment(current ? current.value : null);
 
     await updateValue(id, nextValue);
     return nextValue;
